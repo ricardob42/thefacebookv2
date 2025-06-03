9eb06b23f8d0ef181ac0c3b6a7dbff51ccc92063
ricardob42 <ricardoa.blancor@gmail.com>
2025-06-03 16:14:52 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,20 +2,18 @@ const CACHE_NAME = 'thefacebook-v1';
 const ASSETS = [
   '/',
   '/index.html',
-  '/styles.css',
-  '/script.js',
   '/icons/icon-192.png',
   '/icons/icon-512.png'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
