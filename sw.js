e0daa5702093eea44c8dc52208853fd6e089380f
ricardob42 <ricardoa.blancor@gmail.com>
2025-06-03 15:34:53 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,45 @@
-const CACHE_NAME = 'thefacebook-v1';
-const ASSETS = [
+const CACHE_NAME = 'thefacebook-v3';
+const OFFLINE_URL = '/offline.html';
+const CACHE_FILES = [
   '/',
   '/index.html',
-  '/styles.css',
-  '/script.js',
+  OFFLINE_URL,
   '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  '/css/styles.css',
+  '/js/main.js'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+// Install Phase
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(CACHE_FILES))
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+// Activation
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(
+        keys.map(key => key !== CACHE_NAME && caches.delete(key))
+      )
+    )
   );
+});
+
+// Fetch Strategy
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .catch(() => caches.match(OFFLINE_URL))
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => response || fetch(event.request))
+    );
+  }
 });
